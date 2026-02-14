4062755a62dff2fe418d97686fa88a0363e3c1b5
cliffeby <cliffeby@gmail.com>
2026-02-14 14:14:18 -0500

    correct apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend.azurewebsites.net',
+  apiUrl: 'https://rochwebappeasttwo-cffheedghmhpb7ad.eastus-01.azurewebsites.net/api',
   redirectUri: 'https://brave-tree-00ac3970f.1.azurestaticapps.net/',
   postLogoutRedirectUri: 'https://brave-tree-00ac3970f.1.azurestaticapps.net/login'
 };
